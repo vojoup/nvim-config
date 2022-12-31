b2c01158d1b6b1b327b9080c7ee592ed0ec6e5cc
Vojta Oupicky <voj.oup@gmail.com>
2022-12-31 10:47:48 -0500

    Add signature help while typing

---
+++ lua/user/lsp-signature.lua
@@ -0,0 +1,15 @@
+-- Setup signature help
+local lsp_signature_status_ok, lsp_signature = pcall(require, "lsp_signature")
+if not lsp_signature_status_ok then
+	return
+end
+
+lsp_signature.setup({
+	bind = true, -- This is mandatory, otherwise border config won't get registered.
+	handler_opts = {
+		border = "rounded",
+	},
+	hint_enable = false,
+	floating_window_off_x = 0,
+	floating_window_off_y = 2,
+})
